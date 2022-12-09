0a4590a2ccf0a408ca5b584fefbcf809c1693876
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-09 12:42:15 +0300

    Dz1

---
+++ Dz1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("ВВедите пятизначное число ");
+string number = Console.ReadLine(); 
+
+if (number [0] == number [4] && number [1] == number [3] )
+
+{
+    Console.WriteLine("Yes" );
+    
+}
+    else
+    {
+        Console.WriteLine("No" );
+    }
