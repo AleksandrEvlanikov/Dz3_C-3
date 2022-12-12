539e06197a5db9ac01581fac27fde484bf02c2c1
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-12 14:38:37 +0300

    Dz3

---
+++ Dz3/Program.cs
@@ -0,0 +1,7 @@
+﻿int i,ctr;
+    Console.Write("Введите число : ");
+    ctr= Convert.ToInt32(Console.ReadLine());	
+    for( i=1; i<=ctr; i++)
+    {
+	 Console.Write("{2} \n",i,i, (i*i*i));     
+    }
