a23da49efcc3d3f0df40b9c2d072e9f409c773ec
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-12 14:08:34 +0300

    Dz2

---
+++ Dz2/Program.cs
@@ -0,0 +1,23 @@
+﻿ Double x1, x2, y1, y2, x3, y3, distance;
+ 
+ Console.Write("Введите координаты x1: ");
+x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты x2: ");
+ x2 = Convert.ToDouble(Console.ReadLine());
+
+ Console.Write("Введите координаты x3: ");
+ x3 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты y1: ");
+y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты y2: ");
+y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты y3: ");
+y3 = Convert.ToDouble(Console.ReadLine());
+
+distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)+ Math.Pow(x3 - y3, 2) );
+Console.WriteLine($"Расстояние между двумя точками на плоскости равно {distance}");
+Console.ReadKey();
